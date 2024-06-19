0dedcfb5726cab0597fa57d14a6ccb2bd25dc188
frompaje <yanedwards11@gmail.com>
2024-06-19 11:09:55 -0300

    init e-commercer

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is Running, on port: 3000");
+});
